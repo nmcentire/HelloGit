b906df5edf3bf915910c94fbb5638ccf176a7fe4
Norman McEntire <nmcentire@Normans-MacBook-Pro.local>
2015-04-04 21:31:59 -0700

    Added NSLog functions

--- HelloGit/AppDelegate.m
+++ HelloGit/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"%s", __func__);
     return YES;
 }
 
